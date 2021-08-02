d1f8ba9632ae84b3365bf8cf23804c0bbee227c1
Shihab Chy <cshihab04@gmail.com>
2021-08-02 12:08:10 +0600

    updated index and added new packages

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 4000;
+
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useNewUrlparser: true,
+    useUnifiesTopology: true
+});
+
+//bodyparser setup
+app.use(bodyParser.urlencoded({ extended:true}));
+app.use(bodyParser.json());
+
+routes(app);
 
 app.get('/', (req, res) =>
     res.send(`Node and express server running on port ${PORT}`)
